8d2516cae87fe6ae4a20eda8ccb9a03e2b05024d
daniel martins <freitasdaniel39@gmail.com>
2022-01-26 20:37:11 -0300

    implementing login controller

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,29 +1,75 @@
+using Blog.Data;
+using Blog.Extensions;
+using Blog.Models;
 using Blog.Services;
-using Microsoft.AspNetCore.Authorization;
+using Blog.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureIdentity.Password;
 
-namespace Blog.Controllers
-{
+namespace Blog.Controllers {
     [ApiController]
-    public class AccountController : ControllerBase
-    {
-        [HttpPost("v1/login")]
-        public IActionResult Login([FromServices]TokenService tokenService) {
-            var token = tokenService.GenerateToken(null);
+    public class AccountController : ControllerBase {
+        [HttpPost("v1/accounts")]
+        public async Task<IActionResult> Post(
+            [FromBody]RegisterViewModel model,
+            [FromServices]BlogDataContext context
+        ) {
+            if (!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
 
-            return Ok(token);
+            var user = new User
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Slug = model.Email.Replace("@", "-").Replace(".", "-")
+            };
+
+            var password = PasswordGenerator.Generate(25);
+            user.PasswordHash = PasswordHasher.Hash(password);
+
+            try {
+                await context.Users.AddAsync(user);
+                await context.SaveChangesAsync();
+
+                return Ok(new ResultViewModel<dynamic>(new {
+                    user = user.Email, password
+                }));
+            }
+            catch (DbUpdateException) {
+                return StatusCode(400, new ResultViewModel<string>("Erro ao criar usuário."));
+            } catch {
+                return StatusCode(500, new ResultViewModel<string>("Falha interna no servidor"));
+            }
         }
 
-        [Authorize(Roles = "user")]
-        [HttpGet("v1/user")]
-        public IActionResult GetUser() => Ok(User.Identity.Name);
+        [HttpPost("v1/accounts/login")]
+        public async Task<IActionResult> Login(
+            [FromBody]LoginViewModel model,
+            [FromServices]TokenService tokenService,
+            [FromServices]BlogDataContext context)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
 
-        [Authorize(Roles = "author")]
-        [HttpGet("v1/author")]
-        public IActionResult GetAuthor() => Ok(User.Identity.Name);
+            var user = await context
+                .Users
+                .AsNoTracking()
+                .Include(x => x.Roles)
+                .FirstOrDefaultAsync(x => x.Email == model.Email);
 
-        [Authorize(Roles = "admin")]
-        [HttpGet("v1/admin")]
-        public IActionResult GetAdmin() => Ok(User.Identity.Name);
+            if (user == null)
+                return StatusCode(401, new ResultViewModel<string>("Usuário ou senha inválida"));
+
+            if (!PasswordHasher.Verify(user.PasswordHash, model.Password))
+                return StatusCode(401, new ResultViewModel<string>("Usuario ou senha invalida."));
+            
+            try {
+                var token = tokenService.GenerateToken(user);
+                return Ok(new ResultViewModel<string>(token, errors: null));
+            } catch {
+                return StatusCode(500, new ResultViewModel<string>("Falha interna ao gerar token"));
+            }
+        }
     }
 }
---
+++ Migrations/20220126004619_NullFields.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Migrations
+{
+    public partial class NullFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Image",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bio",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
