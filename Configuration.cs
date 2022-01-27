57feb29bb8aeb2fa87547021687faebcf0c4d3bb
daniel martins <freitasdaniel39@gmail.com>
2022-01-26 21:21:16 -0300

    implementing api_key

--- Configuration.cs
+++ Configuration.cs
@@ -2,6 +2,8 @@ namespace Blog
 {
     public static class Configuration
     { 
-        public static string JwtKey { get; set; } = "ODE5NDQ2NDAtY2IwMy00NjU3LTkxYWMtNmM0ZGExZTFjZDJi";
+        public static string JwtKey = "ODE5NDQ2NDAtY2IwMy00NjU3LTkxYWMtNmM0ZGExZTFjZDJi";
+        public static string ApiKeyName = "api_key";
+        public static string ApiKey = "curso_api_ILTevUM/z0ey3NwCV/unWg==";
     }
 }
