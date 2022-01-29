3d768158beaadcaef76eae6f2198349576850d81
Daniel Martins <freitasdaniel39@gmail.com>
2022-01-29 18:54:45 -0300

    reading by AppSettings

--- Configuration.cs
+++ Configuration.cs
@@ -1,9 +1,15 @@
-namespace Blog
-{
-    public static class Configuration
-    { 
-        public static string JwtKey = "ODE5NDQ2NDAtY2IwMy00NjU3LTkxYWMtNmM0ZGExZTFjZDJi";
-        public static string ApiKeyName = "api_key";
-        public static string ApiKey = "curso_api_ILTevUM/z0ey3NwCV/unWg==";
+namespace Blog;
+
+public static class Configuration { 
+    public static string JwtKey = "ODE5NDQ2NDAtY2IwMy00NjU3LTkxYWMtNmM0ZGExZTFjZDJi";
+    public static string ApiKeyName = "api_key";
+    public static string ApiKey = "curso_api_ILTevUM/z0ey3NwCV/unWg==";
+    public SmtpConfiguration Smtp = new();
+
+    public class SmtpConfiguration {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }
