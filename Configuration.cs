1d7b57ecf1bdcf4d835359d3a35d608d5481bcf5
Daniel Martins <freitasdaniel39@gmail.com>
2022-01-29 19:08:51 -0300

    refac Program class

--- Configuration.cs
+++ Configuration.cs
@@ -4,7 +4,7 @@ public static class Configuration {
     public static string JwtKey = "ODE5NDQ2NDAtY2IwMy00NjU3LTkxYWMtNmM0ZGExZTFjZDJi";
     public static string ApiKeyName = "api_key";
     public static string ApiKey = "curso_api_ILTevUM/z0ey3NwCV/unWg==";
-    public SmtpConfiguration Smtp = new();
+    public static SmtpConfiguration Smtp = new();
 
     public class SmtpConfiguration {
         public string Host { get; set; }
