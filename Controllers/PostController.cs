662b584fa883c4d5fda8af5134ccfe29eff5eba9
Daniel Martins <freitasdaniel39@gmail.com>
2022-02-02 20:22:23 -0300

    implementing paginate

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,4 +1,5 @@
 using Blog.Data;
+using Blog.ViewModels;
 using Blog.ViewModels.Posts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,23 +10,35 @@ namespace Blog.Controllers;
 public class PostController : ControllerBase {
     [HttpGet("v1/post")]
     public async Task<IActionResult> GetAsync(
-        [FromServices]BlogDataContext context
+        [FromServices]BlogDataContext context,
+        [FromQuery] int page = 0,
+        [FromQuery] int pageSize = 25
     ) {
+        int count = await context.Posts.CountAsync();
         var posts = await context
                     .Posts
                     .AsNoTracking()
                     .Include(x => x.Category)
                     .Include(x => x.Author)
-                    // .Select(x => new ListPostsViewModels
-                    // {
-                    //     Id = x.Id,
-                    //     Title = x.Title,
-                    //     Slug = x.Slug,
-                    //     LastUpdateDate = x.LastUpdateDate,
-                    //     Category = x.Category.Name,
-                    //     Author = $"{x.Author.Name} ({x.Author.Email})"
-                    // })
+                    .Select(x => new ListPostsViewModels
+                    {
+                        Id = x.Id,
+                        Title = x.Title,
+                        Slug = x.Slug,
+                        LastUpdateDate = x.LastUpdateDate,
+                        Category = x.Category.Name,
+                        Author = $"{x.Author.Name} ({x.Author.Email})"
+                    })
+                    .Skip(page * pageSize)
+                    .Take(pageSize)
+                    .OrderByDescending(x => x.LastUpdateDate)
                     .ToListAsync();
-        return Ok(posts);
+
+        return Ok(new ResultViewModel<dynamic>(new {
+            total = count,
+            page,
+            pageSize,
+            posts
+        }));
     }
 }
