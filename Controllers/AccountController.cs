4bd88f551844f748953dfe89849dbb5c214bfdc5
Daniel Martins <freitasdaniel39@gmail.com>
2022-02-01 22:10:03 -0300

    image upload

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,9 +1,11 @@
+using System.Text.RegularExpressions;
 using Blog.Data;
 using Blog.Extensions;
 using Blog.Models;
 using Blog.Services;
 using Blog.ViewModels;
 using Blog.ViewModels.Accounts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SecureIdentity.Password;
@@ -79,6 +81,44 @@ namespace Blog.Controllers {
             } catch {
                 return StatusCode(500, new ResultViewModel<string>("Falha interna ao gerar token"));
             }
+        }
+
+        [Authorize]
+        [HttpPost("v1/accounts/upload-image")]
+        public async Task<IActionResult> UploadImage(
+            [FromBody] UploadImageViewModel model,
+            [FromServices] BlogDataContext context
+        ) {
+            var filename = $"{Guid.NewGuid().ToString()}.jpg";
+
+            var data = new Regex(@"^data:image\/[a-z]+;base64,")
+                .Replace(model.Base64Image, "");
+            
+            var bytes = Convert.FromBase64String(data);
+
+            try {
+                await System.IO.File.WriteAllBytesAsync($"wwwroot/images/{filename}", bytes);
+            } catch (Exception ex) {
+                return StatusCode(500, new ResultViewModel<string>("Falha interna no servidor."));
+            }
+
+            var user = await context
+                .Users
+                .FirstOrDefaultAsync(x => x.Email == User.Identity.Name);
+            
+            if (user == null)
+                return NotFound(new ResultViewModel<User>("Usuário não encontrado"));
+
+            user.Image = $"https://localhost:7298/images/{filename}";
+
+            try {
+                context.Users.Update(user);
+                await context.SaveChangesAsync();
+            } catch (Exception ex) {
+                return StatusCode(500, new ResultViewModel<string>("Falha interna no servidor."));
+            }
+
+            return Ok(new ResultViewModel<string>("Imagem alterada com sucesso"));
         }
     }
 }
---
+++ ViewModels/Accounts/UploadImageViewModel.cs
@@ -0,0 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels.Accounts;
+
+public class UploadImageViewModel {
+    [Required(ErrorMessage = "Imagem inválida")]
+    public string Base64Image { get; set; }
+}
